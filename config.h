67db6cf75f01f8433cf9a52334d55f516cdbdd21
Ken McKenzie <slain.12@gmail.com>
2020-04-22 23:42:12 +0200

    Added new modules

--- config.h
+++ config.h
@@ -1,28 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"music",	0,	11}, */
-	{"pacpackages",	0,	8},
-	/* {"crypto",	0,	13}, */
-	/* {"torrent",	20,	7}, */
-	{"news",	0,	6},
-	/* {"memory",	10,	14}, */
-	/* {"cpu",		10,	13}, */
-	/* {"moonphase",	18000,	5}, */
-	{"weather",	18000,	5},
-	{"mailbox",	180,	12},
-	{"volume",	0,	10},
-	{"battery",	5,	3},
-	{"clock",	60,	1},
-	{"internet",	5,	4},
-	{"help-icon",	0,	15},
+	/*Command*/				/*Update Interval*/	/*Update Signal*/
+
+	{ "~/.local/bin/statusbar/torrent",    	20,	    		7},
+	{ "~/.local/bin/statusbar/weather",   	18000,			5},
+	{ "~/.local/bin/statusbar/clock",      	60,			1},
+	{ "~/.local/bin/statusbar/volume ",	0,			10},
+	{ "~/.local/bin/statusbar/internet",	20,	 		2},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '|';
-
-// Have dwmblocks automatically recompile and run when you edit this file in
-// vim with the following line in your vimrc/init.vim:
-
-// autocmd BufWritePost */dwmblocks/config.h !sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
